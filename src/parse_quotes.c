5737701a1c9a442260582652d2510fbfabca86ff
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-04-10 14:32:34 +0200

    quotes : fix $ before quote

--- src/parse_quotes.c
+++ src/parse_quotes.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:26:10 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/04/09 19:19:27 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/04/10 14:28:00 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,19 @@ static int	add_new_slice(t_list **quotes, t_quote **slice)
 	return (0);
 }
 
-static int	add_quoted(t_quote *slice, int *err, char *s)
+static int	add_quoted(t_quote *slice, int *err, char *s, bool dollar)
 {
 	char	*tmp;
 
-	slice->str.s += 1;
-	slice->qtype = 1 + (*s == '\"');
-	tmp = ft_strchr(s + 1, *s);
+	slice->str.s += 1 + dollar;
+	slice->qtype = 1 + (s[dollar] == '\"');
+	tmp = ft_strchr(s + 1 + dollar, s[dollar]);
 	if (tmp == NULL)
 	{
 		*err = ERR_PARSE;
 		return (1);
 	}
-	slice->str.len = tmp - s - 1;
+	slice->str.len = tmp - s - 1 - dollar;
 	return (0);
 }
 
@@ -61,14 +61,14 @@ t_list	*parse_quotes(char *s, int *err)
 		if (*err)
 			return (NULL);
 		*slice = (t_quote){0, (t_str){s, 0}};
-		if (*s == '\'' || *s == '\"')
+		if (s[*s == '$'] == '\'' || s[*s == '$'] == '\"')
 		{
-			if (add_quoted(slice, err, s))
+			if (add_quoted(slice, err, s, *s == '$'))
 				return (quotes);
-			s += slice->str.len + 2;
+			s += slice->str.len + 2 + (*s == '$');
 			continue ;
 		}
-		while (*s && *s != '\'' && *s != '\"')
+		while (*s && s[*s == '$'] != '\'' && s[*s == '$'] != '\"')
 		{
 			s++;
 			slice->str.len += 1;
