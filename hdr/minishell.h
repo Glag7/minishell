dd2a1eb01dd2e706b148e7b6be808031664145b1
Tzvetan Trave <tzvetan.trave@abadcafe.org>
2024-04-21 15:39:17 +0200

    formatted header

--- hdr/minishell.h
+++ hdr/minishell.h
@@ -13,21 +13,25 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/* ----- INCLUDES ----- */
+
+# include "libft.h"
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "libft.h"
+
+/* ----- STRUCTURES & MACROS ----- */
 
 //errors
-# define ERR_AINTNOWAY 3
+# define ERR_AINTNOWAY	3
 //
 
 //parsing
-# define QUOTE 1
-# define DQUOTE 2
+# define QUOTE	1
+# define DQUOTE	2
 
 typedef struct s_quote
 {
@@ -35,10 +39,10 @@ typedef struct s_quote
 	t_str	str;
 }	t_quote;
 
-# define AND 0
-# define OR 1
-# define OPEN 0
-# define CLOSE 1
+# define AND	0
+# define OR	1
+# define OPEN	0
+# define CLOSE	1
 
 enum	e_tok
 {
@@ -52,6 +56,7 @@ enum	e_tok
 	VAR,
 	WDCARD
 };
+
 enum	e_redir
 {
 	IN = 0,
@@ -69,6 +74,7 @@ struct s_redir
 		t_list	*wdcard;
 	};
 };
+
 struct s_hdoc
 {
 	bool	expand;
@@ -89,13 +95,15 @@ typedef struct s_tok
 	};
 }	t_tok;
 
+/* ----- PROTOTYPES & MACROS ----- */
+
 t_list	*parse_quotes(char *s, int *err, int *exc);
 t_list	*parse_pars(t_list *lst, int *err, int *exc);
 //
 
 //output
-# define MSG_QUOTE "minishell: syntax error: open quote\n"
-# define MSG_PAR "minishell: syntax error: bad parenthesis\n"
+# define MSG_QUOTE	"minishell: syntax error: open quote\n"
+# define MSG_PAR	"minishell: syntax error: bad parenthesis\n"
 
 void	ft_perror(char	*s);
 //
@@ -113,4 +121,5 @@ char	**get_var(char **envp, char *var);
 int		replace_var(char **envp, char *new, char *old_name);
 int		remove_var(char ***envp, char *var);
 //
+
 #endif
