1dcd2da346f4314af6824d012429d4a909d09e7e
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-04-23 17:08:04 +0200

    added op parsing

--- src/parse_op.c
+++ src/parse_op.c
@@ -6,34 +6,66 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 20:03:09 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/04/23 16:01:31 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/04/23 17:07:21 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	add_op(t_list **lst, int *i, int j, int type)
+static int	add_op(t_list *curr, int i, int tokn, int type)
 {
-	if (j)
-		return (0);
+	t_list	*tmp;
+	t_tok	*tok;
+
+	if (((t_tok *)curr->content)->quote.str.len - i - 1 - (tokn == OP))
+	{
+		tok = malloc(sizeof(*tok));
+		tmp = ft_lstnew(tok);
+		if (tok == NULL || tmp == NULL)
+		{
+			free(tok);
+			free(tmp);
+			return (1);
+		}
+		*tok = (t_tok){.tok = UNDEF, .quote = (t_quote){0,
+			(t_str){((t_tok *)curr->content)->quote.str.s + i + 1 + (tokn == OP),
+				((t_tok *)curr->content)->quote.str.len - i - 1 - (tokn == OP)}}};
+		ft_lstinsert(&curr, tmp, 0);
+	}
+	tok = ((t_tok *)curr->content);
+	tok->quote.str.len = i;
+	if (i != 0)
+	{
+		tok = malloc(sizeof(*tok));
+		tmp = ft_lstnew(tok);
+		if (tok == NULL || tmp == NULL)
+		{
+			free(tok);
+			free(tmp);
+			return (1);
+		}
+		ft_lstinsert(&curr, tmp, 0);
+	}
+	*tok = (t_tok){.tok = tokn, .type = type};
+	return (0);
 }
 
-static int	search_op(t_list **lst, t_list *curr, int *i)
+static int	search_op(t_list *curr)
 {
 	t_tok	*tok;
-	size_t	j;
+	size_t	i;
 
-	j = 0;
+	i = 0;
 	tok = (t_tok *)curr->content;
-	while (j < tok->quote.str.len)
+	while (i < tok->quote.str.len)
 	{
-		if (tok->quote.str.s[j] == '&' && tok->quote.str.s[j + 1] == '&')
-			return (add_op(lst, &i, j, AND));
-		else if (tok->quote.str.s[j] == '|' && tok->quote.str.s[j + 1] == '|')
-			return (add_op(lst, &i, j, OR));
-		else if (tok->quote.str.s[j] == '|')
-			return (add_op(lst, &i, j, PIPE));
-		j++;
+		if (tok->quote.str.s[i] == '&' && tok->quote.str.s[i + 1] == '&')
+			return (add_op(curr, i, OP, AND));
+		else if (tok->quote.str.s[i] == '|' && tok->quote.str.s[i + 1] == '|')
+			return (add_op(curr, i, OP, OR));
+		else if (tok->quote.str.s[i] == '|')
+			return (add_op(curr, i, PIPE, 80085));
+		i++;
 	}
 	return (0);
 }
@@ -41,22 +73,20 @@ static int	search_op(t_list **lst, t_list *curr, int *i)
 void	parse_op(t_list **lst, int *err, int *exc)
 {
 	t_list	*tmp;
-	size_t	i;
 
-	i = 0;
 	tmp = *lst;
 	while (tmp)
 	{
 		if (((t_tok *)tmp->content)->tok == UNDEF
 			&& ((t_tok *)tmp->content)->quote.qtype == 0
-			&& search_op(lst, tmp, &i))
+			&& search_op(tmp))
 		{
 			*err = ERR_AINTNOWAY;
 			*exc = 2;
 			ft_lstclear(lst, &free);
 			return ;
 		}
-		i++;
 		tmp = tmp->next;
 	}
+	//check
 }
