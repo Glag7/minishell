55a2fa4bcf719c8b1d57b05b82368720b8810d78
Tzvetan Trave <tzvetan.trave@abadcafe.org>
2024-05-01 19:36:11 +0200

    WIP, lacks management of OLDPWD and SHLVL

--- src/dup_envp.c
+++ src/dup_envp.c
@@ -6,14 +6,40 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 16:27:02 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/05/01 17:42:15 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/05/01 19:34:11 by ttrave           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//PWD, le creer
-//SHLEVEL, si existe pas mis a 1, si negatif mis a 0
+//PWD initialise avec getcwd
+//OLDPWD : si pas declare ou pas init : il est declare
+//         si init mais son chemin n'existe pas : il est supprime
+//         si init et chemin existe : il est conserve tel quel
+//SHLEVEL : si pas declare ou pas init ou pas numerique : mis a 1
+//          si init et negatif : mis a 0
+//          si init et positif : incremente de 1
+static int	check_pwd_shlvl(char ***envp_ptr, size_t len)
+{
+	char	*new_pwd;
+
+	new_pwd = getcwd(NULL, 0);
+	if (new_pwd == NULL)
+	{
+		ft_perror("minishell : getcwd() : malloc() : memory allocation failed\n");
+		return (ERR_AINTNOWAY);
+	}
+	old_pwd = get_var(*envp_ptr, "PWD");
+	if (old_pwd != NULL)
+	{
+		free(*old_pwd);
+		*old_pwd = new_pwd;
+	}
+	else
+		(*envp_ptr)[len] = new_pwd;
+	// SHLVL et OLPWD
+	return (0);
+}
 
 int	dup_envp(char ***envp_ptr)
 {
@@ -26,9 +52,10 @@ int	dup_envp(char ***envp_ptr)
 	j = -1;
 	while (envp[i])
 		i++;
-	*envp_ptr = malloc((i + 3) * sizeof(char *));
+	*envp_ptr = malloc((i + 4) * sizeof(char *));
 	if (*envp_ptr == NULL)
 		return (ERR_AINTNOWAY);
+	(*envp_ptr)[i] = NULL;
 	while (++j < i)
 	{
 		(*envp_ptr)[j] = ft_strdup(envp[j]);
@@ -40,5 +67,5 @@ int	dup_envp(char ***envp_ptr)
 		}
 	}
 	(*envp_ptr)[j] = NULL;
-	return (check_pwdshlevel(envp));
+	return (check_pwd_shlvl(envp_ptr, i));
 }
