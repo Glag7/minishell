5a6b2908d73d395ed1cab42f19c8d94dcec1ca5f
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-05-02 20:00:36 +0200

    fixed parsing

--- src/parsing/check_pars.c
+++ src/parsing/check_pars.c
@@ -6,35 +6,29 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 16:55:31 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/04/21 17:00:30 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/05/02 19:50:26 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	check_pars(t_list *pars, ssize_t plevel)
+int	check_pars(t_list *pars)
 {
-	size_t	i;
-	bool	notempty;
-	bool	past_par;
+	t_tok	*tok;
+	ssize_t	plevel;
 
-	past_par = OPEN;
-	while (pars && plevel == 0)
+	plevel = 0;
+	while (pars)
 	{
-		if (((t_tok *)pars->content)->tok == PAR)
+		tok = (t_tok *)pars->content;
+		if (tok->tok == PAR)
 		{
-			if (past_par != ((t_tok *)pars->content)->type && !notempty)
+			plevel += (tok->type == OPEN) - (tok->type == CLOSE);
+			if (plevel < 0)
 				return (1);
-			notempty = 0;
-			past_par = ((t_tok *)pars->content)->type;
-		}
-		else
-		{
-			notempty = !!(((t_tok *)pars->content)->quote.qtype);
-			i = -1;
-			while (++i < ((t_tok *)pars->content)->quote.str.len && !notempty)
-				notempty |= (ft_in(((t_tok *)pars->content)
-							->quote.str.s[i], " \t\n") == -1);
+			if (pars->next && ((t_tok *)pars->next->content)->tok == PAR
+				&& ((t_tok *)pars->next->content)->type != tok->type)
+				return (1);
 		}
 		pars = pars->next;
 	}
