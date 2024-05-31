f46f97cf43c0a37b32dd7a71078fecf7ae2181e7
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-05-31 14:35:58 +0200

    fix make stupid

--- src/common/init_mini.c
+++ src/common/init_mini.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 17:12:19 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/05/31 14:02:12 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/05/31 14:35:05 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ void	init_mini(t_mini *mini, int argc, char **argv, char **envp)
 	(void) argc;
 	(void) argv;
 	rl_catch_signals = 0;
+	rl_outstream = stderr;
 	mini->exc = 0;
 	mini->forked = 0;
 	mini->newpipe[0] = -1;
@@ -60,10 +61,18 @@ void	init_mini(t_mini *mini, int argc, char **argv, char **envp)
 	mini->oldpipe[0] = -1;
 	mini->oldpipe[1] = -1;
 	mini->pids = NULL;
-	mini->hprompt = "icidocument > ";
-	mini->prompt = "coquillage de petite taille > ";
 	if (isatty(0))
+	{
+		mini->hprompt = "icidocument > ";
+		mini->prompt = "coquillage de petite taille > ";
 		rl_event_hook = &event;
+	}
+	else
+	{
+		mini->hprompt = NULL;
+		rl_completion_entry_function = &foo;
+		mini->prompt = NULL;
+	}
 	mini->err = dup_envp(&envp);
 	mini->envp = (t_envp){envp, 1, 1};
 }
