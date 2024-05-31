7794820c759b22f0946d668f9ff445c0f2cd7f7d
Glag7 <glaguyonie@gmail.com>
2024-05-31 11:58:43 +0200

    fixme hdoc

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:53:42 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/05/20 14:41:47 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/05/31 11:58:35 by glag             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ static void	exec_line(t_mini *mini)
 	if (mini->exec == NULL)
 		return ;
 	mini->hdocs = NULL;
-	fill_heredocs(mini->exec, mini);
+	fill_heredocs(mini->exec, mini);//FIXME: must stop on ctrl c
 	if (mini->err == 0)
 		execline(mini, mini->exec);
 	if (mini->forked == 0)
