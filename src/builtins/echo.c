2b227e039f4dbb3d68874672d6031713fe205ce2
Glag7 <glaguyonie@gmail.com>
2024-05-31 22:07:57 +0200

    echo: fix -n + better error messages + 1 write

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: ttrave <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 13:10:32 by ttrave            #+#    #+#             */
-/*   Updated: 2024/05/28 19:09:36 by ttrave           ###   ########.fr       */
+/*   Updated: 2024/05/31 22:05:44 by glag             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ static bool	get_option_nl(char **argv, size_t *i_ptr)
 	option = argv[1];
 	while (option != NULL && option[0] == '-' && option[1] == 'n')
 	{
-		nl = 0;
 		i_option = 2;
 		while (option[i_option] == 'n')
 			i_option++;
 		if (option[i_option] != '\0')
 			break ;
+		nl = 0;
 		i++;
 		option = argv[i];
 	}
@@ -39,35 +39,85 @@ static bool	get_option_nl(char **argv, size_t *i_ptr)
 	return (nl);
 }
 
-static int	write_loop(char **argv, bool nl, size_t i, int fd)
+static char	*join_arg(char *toprint, char **argv, size_t i)
 {
-	char	*arg;
+	char	*tmp;
 
-	arg = argv[i];
-	while (arg != NULL)
+	if (i)
+		tmp = ft_strjoin(toprint, argv[i]);
+	else
+		tmp = ft_strdup(argv[i]);
+	free(toprint);
+	if (tmp == NULL)
 	{
-		if (g_sig == 2)
-		{
-			if (write(fd, "\n", 1) == -1)
-				return (1);
-			return (130);
-		}
-		if (write(fd, arg, ft_strlen(arg)) == -1
-			|| (argv[i + 1] != NULL && write(fd, " ", 1) == -1))
-			return (1);
-		i++;
-		arg = argv[i];
+		ft_perror3("minishell: echo:", strerror(errno), "\n");
+		return (NULL);
 	}
-	if (nl == 1 && write(fd, "\n", 1) == -1)
+	toprint = tmp;
+	if (argv[i + 1])
+	{
+		tmp = ft_strjoin(toprint, " ");
+		free(toprint);
+	}
+	if (tmp == NULL)
+	{
+		ft_perror3("minishell: echo:", strerror(errno), "\n");
+		return (NULL);
+	}
+	return (tmp);
+}
+
+static int	print_arg(char *toprint, int nl, int fd)
+{
+	size_t	len;
+
+	len = ft_strlen(toprint);
+	if (nl == 1)
+		toprint[len++] = '\n';
+	if (write(fd, toprint, len) == -1)
+	{
+		free(toprint);
+		ft_perror3("minishell: echo:", strerror(errno), "\n");
 		return (1);
+	}
+	free(toprint);
 	return (0);
+}
+
+static int	join_loop(char **argv, bool nl, int fd)
+{
+	char	*toprint;
+	size_t	i;
+
+	i = 0;
+	toprint = NULL;
+	while (argv[i])
+	{
+		toprint = join_arg(toprint, argv, i);
+		if (toprint == NULL)
+			return (2);
+		i++;
+	}
+	if (g_sig == 2)
+	{
+		free(toprint);
+		if (write(fd, "\n", 1) == -1)
+		{
+			ft_perror3("minishell: echo:", strerror(errno), "\n");
+			return (1);
+		}
+		return (130);
+	}
+	return (print_arg(toprint, nl, fd));
 }
 
 int	builtin_echo(size_t argc, char **argv, t_envp *envp_status, int *fds)
 {
 	size_t	i;
+	int		nl;
 
 	(void)argc;
 	(void)envp_status;
-	return (write_loop(argv, get_option_nl(argv, &i), i, fds[WRITE]));
+	nl = get_option_nl(argv, &i);
+	return (join_loop(argv + i, nl, fds[WRITE]));
 }
