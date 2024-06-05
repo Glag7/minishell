ab7e0f21a3f72aa9543faa36d089597203a26dee
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-06-05 14:02:40 +0200

    fix hdoc not splitting last word (refactor rm malloc)

--- src/parsing/add_hdoc.c
+++ src/parsing/add_hdoc.c
@@ -6,27 +6,23 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:03:46 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/06/01 19:20:28 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/06/05 14:01:04 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	copy_loop(t_list *lst, t_list *curr, t_str *lim, size_t size)
+static void	end_loop(t_list *lst, t_list *curr, t_str *lim, size_t size)
 {
 	t_list	*tmp;
 	t_tok	*tok;
 
-	tmp = curr;
-	while (size < lim->len)
+	if (size > lim->len)
 	{
 		tok = (t_tok *)curr->content;
-		ft_memcpy(lim->s + size, tok->quote.str.s,
-			ft_min(lim->len - size, tok->quote.str.len));
-		size += ft_min(lim->len - size, tok->quote.str.len);
-		tmp = curr->next;
-		ft_lstdelone(curr, &free);
-		curr = tmp;
+		tok->quote.str.s += tok->quote.str.len - (size - lim->len);
+		tok->quote.str.len = size - lim->len;
+		tmp = curr;
 	}
 	while (curr)
 	{
@@ -41,46 +37,32 @@ static void	copy_loop(t_list *lst, t_list *curr, t_str *lim, size_t size)
 	lim->s[lim->len] = 0;
 }
 
-static inline int	nspc(t_str s, size_t i)
-{
-	size_t	j;
-
-	j = i;
-	while (j < s.len)
-	{
-		if (s.s[j] != ' ' && s.s[j] != '\t' && s.s[j] != '\n')
-			return (1);
-		j++;
-	}
-	return (0);
-}
-
 static int	add_lim(t_list *lst, t_tok *tok, size_t i, t_str *lim)
 {
 	size_t	size;
 	t_list	*tmp;
+	t_list	*curr;
 
 	lim->s = malloc(lim->len + 1);
 	if (lim->s == NULL)
 		return (ERR_AINTNOWAY);
-	size = ft_min(lim->len, tok->quote.str.len - i);
+	size = 0;
 	ft_memcpy(lim->s, tok->quote.str.s + i, size);
-	if (i + lim->len < tok->quote.str.len && nspc(tok->quote.str, i + lim->len))
+	curr = lst->next;
+	while (size < lim->len)
 	{
-		tok = malloc(sizeof(*tok));
-		tmp = ft_lstnew(tok);
-		if (tmp == NULL || tok == NULL)
+		tok = (t_tok *)curr->content;
+		ft_memcpy(lim->s + size, tok->quote.str.s,
+			ft_min(lim->len - size, tok->quote.str.len));
+		size += tok->quote.str.len;
+		if (size <= lim->len)
 		{
-			free(tok);
-			free(tmp);
-			return (ERR_AINTNOWAY);
+			tmp = curr->next;
+			ft_lstdelone(curr, &free);
+			curr = tmp;
 		}
-		ft_lstinsert(&lst, tmp, 0);
-		*tok = (t_tok){.tok = UNDEF, .quote = {.qtype = 0, .str = {((t_tok *)
-				lst->content)->quote.str.s + i + lim->len, ((t_tok *)
-				lst->content)->quote.str.len - i - lim->len}}};
 	}
-	copy_loop(lst, lst->next, lim, size);
+	end_loop(lst, curr, lim, size);
 	return (0);
 }
 
